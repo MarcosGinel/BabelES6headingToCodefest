9e9cf36e89552e71fce9660fd1d7d5bf7135ef19
Marcos Ginel <Marcos.Ginel@gmail.com>
2017-12-19 15:56:59 +0100

    Añadido gulpfile.js y modificaciones al package.json para tener eslint de ES

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp = require('gulp');
+const livereload = require('gulp-livereload');
+const babel = require('gulp-babel');
+
+// Pathfiles
+var DIST_PATH = 'public/dist';
+var SCRIPTS_PATH = 'public/scripts/**/*.js';
+var CSS_PATH = 'public/css/**/*.css';
+
+gulp.task('scripts', function() {
+  return gulp.src('public/scripts/*.js')
+            .pipe(babel({
+                presets: ['es2015']
+            }))
+            .pipe(gulp.dest(DIST_PATH))
+            .pipe(livereload());
+});
+
+gulp.task('default', ['scripts', 'watch'], function() {
+  console.log("Ejecutando la tarea por defecto...");
+});
+
+gulp.task('watch', function() {
+  require('./server.js');
+  livereload.listen();
+  gulp.watch(SCRIPTS_PATH, ['scripts']);
+});
